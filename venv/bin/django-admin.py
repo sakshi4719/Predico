1215adcd0478d702ac2f93264775a97781abc886
sakshi4719 <sakshi.raut@somaiya.edu>
2025-04-23 01:41:40 +0530

    works with better ui

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sakshi/Downloads/Disease-Prediction-using-Django-and-machine-learning-master/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
